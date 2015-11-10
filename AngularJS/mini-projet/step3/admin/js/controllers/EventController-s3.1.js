ac7c642cbc38fa97ff442304635acd9fd97fec0d
Stan Renia <Stan Renia>
2015-11-10 17:18:51 +0100

    Adding step3 functionalities - Still things to do

--- AngularJS/mini-projet/step3/admin/js/controllers/EventController-s3.1.js
+++ AngularJS/mini-projet/step3/admin/js/controllers/EventController-s3.1.js
@@ -3,5 +3,38 @@ angular.module('adminApp').controller('eventCtrl',eventCrtlFnt);
 eventCrtlFnt.$inject=['$scope','$log', '$window' , 'factory'];
 
 function eventCrtlFnt($scope, $log, $window, factory){
+    $scope.currentPresentation = factory.presentationCreation("FirstPres", "My first presentation");
+    var slid_ct = 0, slide_img = 0;
+    var imgs = ["img/Chad_Fullbring.jpg", "img/GuildWars2.jpg", "img/rio_saeba.jpg", ""];
 
+    function init_fortest(){
+        for(var i=0; i<4; i++)
+            $scope.newSlide();
+    }
+    $scope.newSlide = function(){
+        slid_ct++;
+        var img_src = imgs[slide_img];
+        slide_img++;
+
+        var slide = factory.slidCreation("mySlide"+slid_ct, "myTxt");
+        var content = factory.contentCreation("myContentTitle", "myType", img_src);
+        if(slide_img >= 4)
+            slide_img = 0;
+        else
+            slide.contentMap[content.id] = content;
+        $scope.currentPresentation.slidArray.push(slide);
+
+        if($scope.currentSlide == undefined)
+            $scope.currentSlide = slide;
+    };
+
+    $scope.selectCurrentSlid=function(slide){
+        $scope.currentSlide=slide;
+    };
+    $scope.isSlidContentEmpty=function(slid){
+        var contentArray = factory.mapToArray(slid.contentMap);
+        return(contentArray.length == 0);
+    };
+
+    init_fortest();
 }
--- AngularJS/mini-projet/step3/admin/js/services/FactoryService-s3.1.js
+++ AngularJS/mini-projet/step3/admin/js/services/FactoryService-s3.1.js
@@ -1,45 +1,47 @@
-var contentType={}
+var contentType={};
 contentType.IMG_URL="IMG_URL";
 contentType.IMG_B64="IMG_B64";
+
 angular.module('factoryServices', []).factory('factory',factoryFnc);
 function factoryFnc(){
 	var factory = {
 		generateUUID:
-		generateUUID,
+			generateUUID,
 		contentCreation:
-		contentCreation,
+			contentCreation,
 		slidCreation:
-		slidCreation,
+			slidCreation,
 		presentationCreation: presentationCreation,
 		mapToArray:
-		mapToArray
+			mapToArray
 	};
 // http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function generateUUID(){
-	var d = new Date().getTime();
-	var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c)
-		var r = (d + Math.random()*16)%16 | 0;
-		d = Math.floor(d/16);
-		return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-	});
-	return uuid;
-};
-{
-	function contentCreation(title,type,src){
-// TODO
-};
-function slidCreation(title,txt){
-// TODO
-};
-function presentationCreation(title,description){
-// TODO
-};
-function mapToArray(map){
-	contentArray=[];
-	for(key in map){
-		contentArray.push(map[key]);
+	function generateUUID(){
+		var d = new Date().getTime();
+		var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c){
+			var r = (d + Math.random()*16)%16 | 0;
+			d = Math.floor(d/16);
+			return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+		});
+		return uuid;
 	}
-	return contentArray;
-};
-return factory;
-};
+
+	function contentCreation(title,type,src){
+		/* Content Id title src type*/
+		return {id: generateUUID(), title: title, src: src, type: type};
+	}
+	function slidCreation(title,txt){
+		return {id: generateUUID(), title: title, txt: txt, contentMap: {}};
+	}
+	function presentationCreation(title,description){
+		return {id: generateUUID(), title: title, description: description, slidArray: []};
+	}
+	function mapToArray(map){
+		var contentArray=[];
+		for(var key in map){
+			contentArray.push(map[key]);
+		}
+		return contentArray;
+	}
+	return factory;
+}
